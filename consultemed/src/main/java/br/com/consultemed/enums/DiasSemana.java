e9d8f69d4cfcbf3156cf1b453a9e8bcadc72e647
Sergio Diniz <sergiodinizsh@gmail.com>
2019-06-27 23:47:54 -0300

    ajuste no cadastro do dia do medico

--- consultemed/src/main/java/br/com/consultemed/enums/DiasSemana.java
+++ consultemed/src/main/java/br/com/consultemed/enums/DiasSemana.java
@@ -1,5 +1,20 @@
 package br.com.consultemed.enums;
 
 public enum DiasSemana {
-    SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO
+    DOMINGO(1), SEGUNDA(2), TERCA(3), QUARTA(4), QUINTA(5), SEXTA(6), SABADO(7);
+
+    public final int dia;
+
+    DiasSemana(int i) {
+        this.dia = i;
+    }
+
+    public static DiasSemana getDia(int i){
+        for (DiasSemana e : values()) {
+            if (e.dia == i) {
+                return e;
+            }
+        }
+        return null;
+    }
 }
